27d853b8db98b2c763497c0afd7c49a8956f07f9
FonixPython <vlevente.main1123@gmail.com>
2026-08-24 08:05:05 +0200

    added routes from userActionController into routes.ts

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import { login, register, logout, checkAuth } from './Controllers/userController.js';
+import { addTransaction, deleteTransaction, editTransaction, getBalances, getCoffeeVariations, getTransactions } from './Controllers/userActionController.js';
 
 export const router = express.Router();
 
@@ -14,3 +15,15 @@ router.post("/login", login)
 router.post("/register", register)
 router.get("/logout", logout)
 router.get("/verify", checkAuth)
+
+// Actions for users
+
+router.get("/api/getBalances", getBalances)
+router.get("/api/getTransactions/:poolId", getTransactions)
+router.get("/api/getVariations", getCoffeeVariations)
+router.post("/api/addTranaction", addTransaction)
+router.post("/api/editTransaction/:transactionId", editTransaction)
+router.get("/api/deleteTransaction/:transactionId", deleteTransaction)
+
+// Actions for admin
+
